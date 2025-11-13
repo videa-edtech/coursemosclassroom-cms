a9c417ff4d8b2d019e689afcd7b09fd143a25270
oscarluu1506 <oscar@videabiz.com>
2025-11-13 10:24:45 +0700

    done: integrated theme

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,59 +1,26 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
+import { Background } from "@/components/background";
+import { FAQ } from "@/components/blocks/faq";
+import { Features } from "@/components/blocks/features";
+import { Hero } from "@/components/blocks/hero";
+import { Logos } from "@/components/blocks/logos";
+import { Pricing } from "@/components/blocks/pricing";
+import { ResourceAllocation } from "@/components/blocks/resource-allocation";
+import { Testimonials } from "@/components/blocks/testimonials";
 
-import config from '@/payload.config'
-import './styles.css'
-
-export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
+export default function Home() {
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
-    </div>
-  )
+    <>
+      <Background className="via-muted to-muted/80">
+        <Hero />
+        <Logos />
+        <Features />
+        <ResourceAllocation />
+      </Background>
+      <Testimonials />
+      <Background variant="bottom">
+        <Pricing />
+        <FAQ />
+      </Background>
+    </>
+  );
 }
